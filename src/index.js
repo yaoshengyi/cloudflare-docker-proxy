5960de2514c16f5198579e9848a3c9fa8fd02f78
yaoshengyi <35094285+yaoshengyi@users.noreply.github.com>
2024-06-23 19:46:14 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.20241231.xyz": dockerHub,
+  "quay.20241231.xyz": "https://quay.io",
+  "gcr.20241231.xyz": "https://gcr.io",
+  "k8s-gcr.20241231.xyz": "https://k8s.gcr.io",
+  "k8s.20241231.xyz": "https://registry.k8s.io",
+  "ghcr.20241231.xyz": "https://ghcr.io",
+  "cloudsmith.20241231.xyz": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.20241231.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
